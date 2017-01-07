9ccf66697fb41ed25b94ddec80d912d3d5d2e63a
srinivaskonda <srinivaskonda@hotmail.com>
2017-01-07 17:42:28 +0000

    Add files via upload
    Initial Version of the shopping Cart.java file

---
+++ onlineShoppingCart.java
@@ -0,0 +1,21 @@
+package sri;
+
+import java.util.List;
+
+public class onlineShoppingCart {
+	//variable to calculate the number of apples to be billed
+		double totalApplesBilled = 0;
+		//variable to calculate the number of oranges to be billed
+		double totalOrangesBilled = 0;
+		//list of all the products
+		List<String> products;
+	 // to get the number of products
+		public int getTotalProducts() {
+			return products.size();
+		}
+		public String getTotal() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+}
