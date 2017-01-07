f4579ebd9750b1a0011b001f753a49dcb7d31a93
srinivaskonda <srinivaskonda@hotmail.com>
2017-01-07 18:04:36 +0000

    Add files via upload
    Added the test cases and code for the special offers

--- onlineShoppingCart.java
+++ onlineShoppingCart.java
@@ -1,20 +1,33 @@
 package sri;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class onlineShoppingCart {
 	//variable to calculate the number of apples to be billed
-		double totalApplesBilled = 0;
-		//variable to calculate the number of oranges to be billed
-		double totalOrangesBilled = 0;
-		//list of all the products
-		List<String> products;
-	 // to get the number of products
-		public int getTotalProducts() {
-			return products.size();
-		}
-		public double getTotal() {
-			
+	double totalApplesBilled = 0;
+	//variable to calculate the number of oranges to be billed
+	double totalOrangesBilled = 0;
+	//list of all the products
+	List<String> products;
+
+	public onlineShoppingCart() {
+
+		//empty arrayList of products 
+		products = new ArrayList<String>();
+
+	}
+       // method to add products
+	public void add(String product) {
+		products.add(product);
+	}
+      // to get the number of products
+	public int getTotalProducts() {
+		return products.size();
+	}
+
+	//get the total amount to be paid
+	public double getTotal() {
 		double total = 0;	
 
 		int i = 0;
@@ -35,13 +48,48 @@ public class onlineShoppingCart {
 				totalOrangesBilled += 1;
 			}
 
-		
-		
+		}
+		//if the totalOrangesbilled are more than 1 then use this code
+		//special offer for orange buy 3 get 1 free
+		if (totalOrangesBilled > 1) {
+			if (totalOrangesBilled % 3 == 0) {
+				
+				totalOrangesBilled = (totalOrangesBilled/3)*2;
+						
+			}
+			else if(totalOrangesBilled % 3 == 1) {
+					totalOrangesBilled = (((totalOrangesBilled-1)/3)*2)+1;
+				}
+			else if(totalOrangesBilled % 3 == 2) {
+				totalOrangesBilled = (((totalOrangesBilled-2)/3)*2)+2;
+			}
+		}
+		//if totalOrangesBilled <=1 use this code
+		total = total + ((totalOrangesBilled) * 0.25);
+
+		//special offer for Apples 
+		if (totalApplesBilled % 2 == 0) {
+
+			totalApplesBilled = totalApplesBilled*0.5;
+			total = total + (totalApplesBilled *0.60);
+		} else {
+			totalApplesBilled = (totalApplesBilled-1) * 0.5;
+			total = total + (totalApplesBilled*0.60)+0.60;
+		}
 
 		
-		
-		
-}
 		return total;
-}
+	}
+
+	public static void main(String s[]) {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("apple");		
+		onlineShoppingcart.add("orange");
+		
+		double totalBill = 0;
+		totalBill = onlineShoppingcart.getTotal();
+		System.out.println("Total Products :"+onlineShoppingcart.getTotalProducts());
+		System.out.println(onlineShoppingcart.products);
+		System.out.println(totalBill);
+	}
 }
--- onlineShoppingTest.java
+++ onlineShoppingTest.java
@@ -2,6 +2,7 @@ package sri;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class onlineShoppingTest {
@@ -14,5 +15,80 @@ public class onlineShoppingTest {
 		assertEquals(onlineShoppingcart.getTotal(),0.0,0.0);
 		
 	}
+	@Test
+	public void testCartApplesOranges() {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
 
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		assertEquals(onlineShoppingcart.getTotal(), 3.9d, 0);
+
+	}
+
+	@Test
+	public void testCartApples() {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		assertEquals(onlineShoppingcart.getTotal(), 2.4d, 0);
+
+	}
+	@Test
+	public void testCart3Oranges() {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		assertEquals(onlineShoppingcart.getTotal(), 0.50d, 0);
+
+	}
+
+	@Test
+	public void testCart1Apple1Orange() {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("orange");
+		assertEquals(onlineShoppingcart.getTotal(), 0.85d, 0);		
+		
+		
+
+	}
+	@Test
+	public void testCart2Apples1Orange() {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("orange");
+		assertEquals(onlineShoppingcart.getTotal(), 0.85d, 0);
+
+	}
+	@Test
+	public void testCart2Apples3Orange() {
+		onlineShoppingCart onlineShoppingcart = new onlineShoppingCart();
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("apple");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		onlineShoppingcart.add("orange");
+		assertEquals(onlineShoppingcart.getTotal(), 1.10d, 0);
+
+	}
 }
